14a87adfaa9e6cdeb2b28d8852d24a3821349056
tajmuhammed123 <muhammedtaj7@gmail.com>
2026-02-05 10:04:21 +0530

    new

--- src/models/Vote.js
+++ src/models/Vote.js
@@ -2,9 +2,27 @@ import mongoose from "mongoose";
 
 const VoteSchema = new mongoose.Schema(
   {
-    valentineId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", required: true },
-    voterIp: { type: String, trim: true, required: true },
-    deviceId: { type: String, trim: true, required: true, unique: true }
+    valentineId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+      required: true,
+      index: true,
+    },
+
+    voterIp: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+
+    deviceId: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      immutable: true,
+      index: true,
+    },
   },
   { timestamps: true }
 );
